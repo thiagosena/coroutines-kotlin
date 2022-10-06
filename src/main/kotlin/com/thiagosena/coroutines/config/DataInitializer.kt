9cc6583565e65e932ff4201a589cfb83a26b735e
Thiago Sena <lord.sena@gmail.com>
2022-10-06 12:55:55 -0300

    feat: create data initializer

---
+++ src/main/kotlin/com/thiagosena/coroutines/config/DataInitializer.kt
@@ -0,0 +1,23 @@
+package com.thiagosena.coroutines.config
+
+import com.thiagosena.coroutines.repositories.PostRepository
+import kotlinx.coroutines.runBlocking
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+
+@Component
+class DataInitializer(private val postRepository: PostRepository) {
+
+    @EventListener
+    fun init() {
+        println("Start data initialization")
+
+        runBlocking {
+            val deleted = postRepository.deleteAll()
+            println("$deleted posts removed")
+            postRepository.init()
+        }
+
+        print("done data initialization")
+    }
+}
