8d07ec92bccd6c41766786db49f7afeae83c944c
Thiago Sena <lord.sena@gmail.com>
2022-10-06 13:17:37 -0300

    refact: adjust event listener to data initializer

--- src/main/kotlin/com/thiagosena/coroutines/config/DataInitializer.kt
+++ src/main/kotlin/com/thiagosena/coroutines/config/DataInitializer.kt
@@ -2,13 +2,14 @@ package com.thiagosena.coroutines.config
 
 import com.thiagosena.coroutines.repositories.PostRepository
 import kotlinx.coroutines.runBlocking
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class DataInitializer(private val postRepository: PostRepository) {
 
-    @EventListener
+    @EventListener(value = [ApplicationReadyEvent::class])
     fun init() {
         println("Start data initialization")
 
